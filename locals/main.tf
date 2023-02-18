8cc68073517a4280e4b6355a3031a8728b118963
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-02-18 10:57:24 +0530

    tf_locals

---
+++ locals/main.tf
@@ -0,0 +1,29 @@
+variable "components" {
+
+    default = {
+        cart= {
+            name = "cart",
+            instance_type = "t3.micro"
+        }
+
+        catalogue = {
+            name = "catalogue",
+            instance_type = "t3.micro"
+
+        }
+    }
+  
+}
+
+locals {
+  instance_type = {for k,v in var.components: k=> v.instance_type }
+  instance_type1 = {for k,v in var.components: k=> v.instance_type1 }
+}
+
+output "instance_type" {
+  value = local.instance_type
+}
+
+output "instance_type1" {
+  value = local.instance_type1
+}
