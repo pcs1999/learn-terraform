0528a262b41093f6ae3ce70570bec1e02fd33214
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-04-20 19:45:53 +0530

    learn tf

--- 04-resource/sample.tf
+++ 04-resource/sample.tf
@@ -12,6 +12,7 @@ data "aws_ami" "example" {
 resource "aws_instance" "firstec2" {
   ami = data.aws_ami.example.id
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
 
   tags = {
     Name= "automachine"
@@ -23,5 +24,32 @@ terraform {
     bucket = "terraform-19feb"
     key    = "apirl/terraform.tfstate"
     region = "us-east-1"
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
   }
 }
