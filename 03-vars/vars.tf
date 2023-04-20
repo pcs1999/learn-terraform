145113a9d95dccdda5c1f45e86b191c0536ef919
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-04-20 18:31:55 +0530

    learn tf

--- 03-vars/vars.tf
+++ 03-vars/vars.tf
@@ -5,3 +5,62 @@ variable "sample" {
 output "location" {
   value = var.sample
 }
+
+#data types
+# 1. string 2.number 3.boolean
+
+#string data types
+variable "sample1" {
+  default = "chandra"
+}
+
+#number data type
+variable "sample2" {
+  default = 100
+}
+
+#boolean data type
+variable "sample3" {
+  default = true
+}
+
+#terraform doesn't support yes/no
+
+#variable types 1.default 2.map 3.list
+
+#default variable type
+variable "sample4" {
+   default = "pcs"
+ }
+
+# list variable type
+variable "sample5" {
+  default = [
+   "hello",
+    1000,
+    true
+  ]
+}
+
+# map variable type
+variable "sample6" {
+  default = {
+    number = 100
+    string = "hello"
+
+    boolean = true
+  }
+}
+
+# accessing the list variable
+output "sample5" {
+  value = var.sample5[0]
+}
+
+# accessing the map variable
+output "sample6" {
+  value = var.sample6["string"]
+}
+
+#input
+variable "chandra" {}
---
+++ 04-resource/sample.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_ami" "example" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
+
+}
+
+resource "aws_instance" "firstec2" {
+  ami = data.aws_ami.example.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name= "automachine"
+  }
+}
