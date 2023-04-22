96bea6b0fb3a0968febd5b14ef5eb515c7792cec
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-04-22 13:19:45 +0530

    learn tf

---
+++ for-each/main.tf
@@ -0,0 +1,32 @@
+resource "aws_instance" "firstec2" {
+  for_each = var.component
+  ami = data.aws_ami.example.id
+  instance_type = each.value.instance_type
+  tags = {
+    Name= each.value["name"]
+  }
+}
+data "aws_ami" "example" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
+
+}
+
+variable "component" {
+  default = {
+    cart ={
+      name= "cart"
+      instance_type= "t3.micro"
+    }
+    cart ={
+      name= "catalogue"
+      instance_type= "t2.micro"
+    }
+  }
+}
+
+
+output "publicip" {
+  value = aws_instance.firstec2.*.public_ip
+}
