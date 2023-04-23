6dda78d4b8560637f54da69e0471fa6371f3d9e2
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-04-23 13:12:28 +0530

    learn tf

--- 10-provisioners/main.tf
+++ 10-provisioners/main.tf
@@ -12,7 +12,6 @@ data "aws_ami" "example" {
 resource "aws_instance" "firstec2" {
   ami = data.aws_ami.example.id
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.allow_tls.id]
 
   tags = {
     Name= "automachine"
@@ -35,29 +34,3 @@ resource "null_resource" "provision" {
   }
 }
 
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
-
-
-  ingress {
-    description      = "TLS from VPC"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "allow_tls"
-  }
-}
